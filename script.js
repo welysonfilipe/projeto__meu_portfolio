36b4d06a98ed9523882714839047896d90a126e1
welysonfilipe <65021357+welysonfilipe@users.noreply.github.com>
2023-10-21 16:00:32 -0300

    Add files via upload
    Criação de um portfólio para avaliação da matéria de Fundamentos de Desenvolvimento Web. Curso Análise e Desenvolvimento de Sistemas.

---
+++ script.js
@@ -0,0 +1,13 @@
+ function limpar() {
+        // Obtém os elementos de entrada pelo ID
+        var camponome = document.getElementById('nome');
+        var campoemail = document.getElementById('email');
+        var campotelefone = document.getElementById('telefone');
+        
+        // Limpa os valores dos campos
+        camponome.value = '';
+        campoemail.value = '';
+        campotelefone.value = '';
+    }
+
+
